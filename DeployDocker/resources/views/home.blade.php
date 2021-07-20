7061e7ec3fa19bb3cce4d0cc98d3a7a64fa17ea3
Zachary Astree <zacharyastree@gmail.com>
2021-07-20 13:24:27 -0400

    Changes made

---
+++ DeployDocker/resources/views/home.blade.php
@@ -0,0 +1,35 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-3 p-5">
+            <img src="img/cham.jpg" alt="" class="rounded-circle" style= "height:150px;">
+        </div>
+        <div class="col-9 pt-5">
+            <div><h1>Username</h1></div>
+            <div class="d-flex">
+                <div class="pr-5"><strong>192</strong> posts</div>
+                <div class="pr-5"><strong>201</strong> followers</div>
+                <div class="pr-5"><strong>132</strong> following</div>
+            </div>
+            <div class="pt-4 font-weight-bold">myfau.fau.edu</div>
+            <div>I'd like to order one large phone with extra phones please. cell phone, no no no rotary... and payphone on half. Sum-Sum, let’s go! Grandpa’s concern for your safety is fleeting! What is this, 90's Conan? Oh no. I'm late to class, bitch. </div>
+            <div><a href="#">www.myfau.fau.edu</a></div>
+        </div>
+    </div>
+
+    <div class="row pt-5">
+        <div class="col-4">
+            <img src="img/macrons.jpg" class="w-100" style= "height:350px; width=250px;" alt="">
+        </div>
+        <div class="col-4">
+            <img src="img/eggs.jpg" class="w-100" style= "height:350px; width=150px;" alt="">
+        </div>
+        <div class="col-4">
+            <img src="img/saucers.jpg" class="w-100" style= "height:350px; width=250px;" alt="">
+        </div>
+    </div>
+
+</div>
+@endsection
